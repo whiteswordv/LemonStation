ac10eb9d7beda561ca25a215261a7bb21e6eb548
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-03-02 23:32:53 -0500

    burshless

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, jsonify, request, send_file
 from networktables import NetworkTables
 
 app = Flask(__name__, static_url_path="")
 
-def set_team(number): 
+
+def set_team(number):
     NetworkTables.setServerTeam(number)
+
 
 set_team(308)
 NetworkTables.initialize()
 table = NetworkTables.getTable("LemonStation")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return send_file('static/index.html')
+    return send_file("static/index.html")
+
 
 @app.route("/motors")
-def motors(): 
+def motors():
     sub_tables = table.getSubTables()
     json_data = []
 
@@ -23,22 +27,24 @@ def motors():
         sub_table = table.getSubTable(sub_name)
 
         json_data.append(
-        {
-            "id": int(sub_name),
-            "brushless": sub_table.getBoolean("bushless", False), 
-            "type": sub_table.getString("motorType", ""),
-            "speed": sub_table.getNumber("speed", 0), 
-            # "state": sub_table.getString("break", "Unknown")
-        })
-    
+            {
+                "id": int(sub_name),
+                "brushless": sub_table.getBoolean("bushless", False),
+                "type": sub_table.getString("motorType", ""),
+                "speed": sub_table.getNumber("speed", 0),
+                # "state": sub_table.getString("break", "Unknown")
+            }
+        )
 
     return jsonify(json_data)
+
 
 @app.post("/speed/<int:id>")
 def set_motor_speed(id):
     speed = request.args.get("speed") or 0
 
     table.getSubTable(str(id)).putNumber("speed", speed)
+
 
 if __name__ == "__main__":
     app.run()
--- static/index.js
+++ static/index.js
@@ -3,174 +3,153 @@ const warning = document.getElementById("warning");
 const motorMenu = document.getElementById("motor-menu");
 
 function createTile(motor) {
+  let tile = document.createElement("div");
+  tile.classList.add("motor-tile");
 
-    let tile = document.createElement("div");
-    tile.classList.add("motor-tile");
+  let name = document.createElement("h1");
+  name.innerHTML = getDisplay(motor);
 
-    let name = document.createElement("h1");
-    name.innerHTML = getDisplay(motor);
+  let id = document.createElement("h2");
+  id.innerHTML = "CAN ID " + motor.id;
 
-    let id = document.createElement("h2");
-    id.innerHTML = "CAN ID " + motor.id;
+  let img = document.createElement("img");
+  img.setAttribute("src", getImage(motor));
 
-    let img = document.createElement("img");
-    img.setAttribute("src", getImage(motor));
+  tile.append(name);
+  tile.append(id);
+  tile.append(img);
 
+  tile.addEventListener("click", () => {
+    settings(motor);
+  });
 
-    tile.append(name);
-    tile.append(id);
-    tile.append(img);
-
-    tile.addEventListener("click", () => {
-        settings(motor);
-    });
-
-    tileGrid.append(tile);
+  tileGrid.append(tile);
 }
 
 function settings(motor) {
+  motorMenu.classList.remove("hidden");
+  tileGrid.classList.add("hidden");
 
-    motorMenu.classList.remove("hidden");
-    tileGrid.classList.add("hidden");
+  let id = motor.id;
 
-    let id = motor.id;
+  let nameHeader = document.getElementById("motor-name");
+  nameHeader.innerHTML = getDisplay(motor);
 
-    let nameHeader = document.getElementById("motor-name");
-    nameHeader.innerHTML = getDisplay(motor);
+  let idHeader = document.getElementById("motor-id");
+  idHeader.innerHTML = "CAN id " + id;
 
-    let idHeader = document.getElementById("motor-id");
-    idHeader.innerHTML = "CAN id " + id;
+  if (getDisplay(motor) == "SPARK MAX") {
+    let dropDown = document.createElement("input");
+    dropDown.attributes = document.createAttribute("type");
+    dropDown.attributes = document.createAttribute("id");
 
-    if (getDisplay(motor) == "SPARK MAX") {
+    dropDown.innerHTML = "<a brushed </a> <a brushless </a>";
 
-        let dropDown = document.createElement("input")
-        dropDown.attributes = document.createAttribute("type");
-        dropDown.attributes = document.createAttribute("id");
-
-        dropDown.innerHTML = "";
-
-        dropDown.addEventListener("input", () => {
-            if (dropDown.value == "brushed")
-                motor.brushless = false;
-            else
-                motor.brushless = true;
-        });
-
-    }
-
-    // let motorImage = document.getElementById("motor-img");
-    // motorImage.setAttribute("src", getImage(motor));
-
-    let back = document.getElementById("back-button");
-
-    let slider = document.getElementById("speed-slider");
-    slider.value = motor.speed;
-
-
-    slider.addEventListener("input", () => {
-
-        let speed = Number(slider.value);
-
-        fetch("/speed/" + id, {
-            method: "POST",
-            body: JSON.stringify({
-                speed: speed,
-            })
-        });
+    dropDown.addEventListener("input", () => {
+      if (dropDown.value == "brushed") motor.brushless = false;
+      else motor.brushless = true;
     });
+  }
 
-    back.addEventListener("click", () => {
-        tileGrid.classList.remove("hidden");
-        motorMenu.classList.add("hidden");
+  // let motorImage = document.getElementById("motor-img");
+  // motorImage.setAttribute("src", getImage(motor));
+
+  let back = document.getElementById("back-button");
+
+  let slider = document.getElementById("speed-slider");
+  slider.value = motor.speed;
+
+  slider.addEventListener("input", () => {
+    let speed = Number(slider.value);
+
+    fetch("/speed/" + id, {
+      method: "POST",
+      body: JSON.stringify({
+        speed: speed,
+      }),
     });
+  });
 
+  back.addEventListener("click", () => {
+    tileGrid.classList.remove("hidden");
+    motorMenu.classList.add("hidden");
+  });
 }
 
 function removeTile(motor) {
-
-    [...tileGrid.querySelectorAll(".motor-tile",)].forEach(tile => {
-        if (tile.querySelector("h2").innerHTML == "CAN ID " + motor.id) {
-            tile.remove();
-        }
-    });
-
+  [...tileGrid.querySelectorAll(".motor-tile")].forEach((tile) => {
+    if (tile.querySelector("h2").innerHTML == "CAN ID " + motor.id) {
+      tile.remove();
+    }
+  });
 }
 
 function getDisplay(motor) {
-    switch (motor.type) {
-        case "sparkmax":
-            return "SPARK MAX"
-        case "krakenx60":
-            return "Kraken X60"
-        case "krakenx44":
-            return "Kraken X44"
-        case "falcon500":
-            return "Falcon 500"
-        default:
-            return "Unknown"
-    }
+  switch (motor.type) {
+    case "sparkmax":
+      return "SPARK MAX";
+    case "krakenx60":
+      return "Kraken X60";
+    case "krakenx44":
+      return "Kraken X44";
+    case "falcon500":
+      return "Falcon 500";
+    default:
+      return "Unknown";
+  }
 }
 
 function getImage(motor) {
-    switch (motor.type) {
-        case "sparkmax":
-            return "imgs/sparkmax.png"
-        case "krakenx60":
-            return "imgs/krakenX60.png"
-        case "krakenx44":
-            return "imgs/krakenX44.png"
-        case "falcon500":
-            return "imgs/falcon500.png"
-        default:
-            return "imgs/placeHolder.png"
-    }
+  switch (motor.type) {
+    case "sparkmax":
+      return "imgs/sparkmax.png";
+    case "krakenx60":
+      return "imgs/krakenX60.png";
+    case "krakenx44":
+      return "imgs/krakenX44.png";
+    case "falcon500":
+      return "imgs/falcon500.png";
+    default:
+      return "imgs/placeHolder.png";
+  }
 }
-
-
 
 let oldMotors = [];
 let motors = [
-    // {
-    //     id: 0,
-    //     type: "sparkmax",
-    //     speed: 0,
-    // },
-
-    // {
-    //     id: 1,
-    //     type: "krakenx60",
-    //     speed: 0,
-    // }
+  // {
+  //     id: 0,
+  //     type: "sparkmax",
+  //     speed: 0,
+  // },
+  // {
+  //     id: 1,
+  //     type: "krakenx60",
+  //     speed: 0,
+  // }
 ];
 
 function includesMotor(array, motor) {
-    return array.some(m => m.id == motor.id && m.type == motor.type);
+  return array.some((m) => m.id == motor.id && m.type == motor.type);
 }
 
 function updateMotors() {
+  // getting the data from the flask local host
+  fetch("/motors")
+    .then((response) => response.json())
+    .then((data) => (motors = data));
 
-    // getting the data from the flask local host
-    fetch("/motors")
-        .then(response => response.json())
-        .then(data => motors = data);
+  // creating the tiles without repeats Uwu
+  motors.forEach((motor) => {
+    if (includesMotor(oldMotors, motor)) return;
 
-    // creating the tiles without repeats Uwu
-    motors.forEach(motor => {
-        if (includesMotor(oldMotors, motor))
-            return;
+    createTile(motor);
+  });
 
-        createTile(motor);
-    });
+  oldMotors.forEach((oldMotor) => {
+    if (!includesMotor(motors, oldMotor)) removeTile(oldMotor);
+  });
 
-
-    oldMotors.forEach(oldMotor => {
-
-        if (!includesMotor(motors, oldMotor))
-            removeTile(oldMotor);
-
-    });
-
-    oldMotors = [...motors];
+  oldMotors = [...motors];
 }
 
 updateMotors();
