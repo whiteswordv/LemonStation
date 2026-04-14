644cd04cc3c31b7600d8041c3a9e6c8b71713f29
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-04-14 16:01:32 -0400

    a

--- frontLemon/src/app.ts
+++ frontLemon/src/app.ts
@@ -1,10 +1,30 @@
 export const localhost = "http://127.0.0.1:5000";
 
+const warningContainer = document.getElementById("warning-container");
+const contentContainer = document.getElementById("content");
 const navContainer = document.getElementById("nav-container");
 const tabContainer = document.getElementById("tab-container");
+
 const addTabButton = document.getElementById("add-button");
 
-const warningContainer = document.getElementById("warning-container");
+export function createError(message: string) {
+
+    const error = document.createElement("div")
+
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = message;
+
+    error.append(errorMessage);
+
+    const closeButton = document.createElement("button");
+    closeButton.innerText = "X";
+
+    closeButton.addEventListener("click", () => error.remove());
+
+
+    warningContainer?.append(error);
+}
+
 
 setInterval(() => {
     // checking if its connected
@@ -12,5 +32,7 @@ setInterval(() => {
         .then((res) => res.json())
         .then((data) => {
             const connected: boolean = data;
+
+
         });
 }, 200);
--- frontLemon/src/main.ts
+++ frontLemon/src/main.ts
@@ -1,5 +1,5 @@
 import { mount } from 'svelte'
-import './app.css'
+import './style/style.css'
 import App from './App.svelte'
 
 const app = mount(App, {
